030feda43b4c5a8bb77b856923b35e278b0d7878
unknown <herrera05luis@gmail.com>
2022-07-18 18:38:45 -0400

    Web view finished

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-const data = {
+/* const data = {
   labels: ["1", "1", "1", "1", "1", "1", "1"],
   datasets: [
     {
@@ -15,13 +15,86 @@ const config = {
   data: data,
   options: {
     scales: {
+      display: false,
+      legend: {
+        display: false, //This will do the task
+      },
       y: {
-        beginAtZero: false,
+        beginAtZero: true,
         display: false,
       },
     },
-    datasets: {},
   },
 };
 
 const myChart = new Chart(document.getElementById("myChart"), config);
+ */
+
+let arr = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
+
+let test = [];
+let bigger = 0;
+let table = document.querySelector("#custom__chart tbody");
+
+arr.forEach((item) => {
+  if (bigger < item.amount) {
+    bigger = item.amount;
+  }
+  test.push(item.amount);
+});
+
+let sizes = [];
+test.forEach((item) => {
+  sizes.push((item * 100) / bigger / 100);
+});
+
+arr.forEach((item, index) => {
+  table.innerHTML += `<tr>
+  <th scope="row">${item.day}</th>
+  <td style="--start: 0; --size: ${sizes[index]}">
+    <span class="tooltip"> $ ${item.amount} </span>
+  </td>
+</tr>`;
+});
+
+let els = document.querySelectorAll("#custom__chart tbody td");
+let lastItem;
+
+els.forEach((item) => {
+  item.addEventListener("click", function () {
+    if (lastItem) {
+      lastItem.classList.toggle("active");
+    }
+    item.classList.toggle("active");
+    lastItem = item;
+  });
+});
